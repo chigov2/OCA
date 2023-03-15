28c15adcbca5fa185ed9d19b11dabd2ecbbffa9c
mikee <chigov@gmail.com>
2023-03-15 14:05:07 +0200

    homework4_1

--- src/main/java/HomeWork/Student.java
+++ src/main/java/HomeWork/Student.java
@@ -8,13 +8,17 @@ public class Student {
     float sredeeGeometry;
     float sredeeHistory;
 
-    void sredneeArifmeticheskoe(float algebra, float geometry, float history) {
-        float srednee = (algebra + geometry + history) / 3;
-        System.out.println(srednee);;
-    }
+
 }
 
 class NewStudent{
+    float sredneeArifmeticheskoe(Student st) {
+        float srednee = (st.sredeeAlgebra+st.sredeeGeometry+st.sredeeHistory) / 3;
+        System.out.println(srednee);
+        return srednee;
+    }
+
+
     public static void main(String[] args) {
         Student student1 = new Student();
         student1.id = 1;
@@ -23,7 +27,10 @@ class NewStudent{
         student1.sredeeAlgebra = 3.5f;
         student1.sredeeGeometry = 4.2f;
         student1.sredeeHistory = 4.5f;
-        student1.sredneeArifmeticheskoe(student1.sredeeAlgebra,student1.sredeeGeometry,student1.sredeeHistory);
+
+        NewStudent newStudent = new NewStudent();
+        newStudent.sredneeArifmeticheskoe(student1);
+
         System.out.println(student1.id + " " + student1.name + " " + student1.lastName + " ");
 
     }
