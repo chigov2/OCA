d6018022d55913020d3ecb56bcf1a5e017b1a9fa
mikee <chigov@gmail.com>
2023-03-28 15:04:42 +0300

    homework13

--- src/main/java/HomeWork/Lesson13/Month.java
+++ src/main/java/HomeWork/Lesson13/Month.java
@@ -17,7 +17,8 @@ public class Month {
     }
 
     public static void main(String[] args) {
-        int month = 1;
-        dayInMonth(6);
+//        int month = 1;
+        Month.dayInMonth(1);
+        Month.dayInMonth(2);
     }
 }
