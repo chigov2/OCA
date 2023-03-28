ce78e8cf7802eb62577db98e492424d7a02064c4
mikee <chigov@gmail.com>
2023-03-28 14:46:39 +0300

    homework13

---
+++ src/main/java/HomeWork/Lesson13/Month.java
@@ -0,0 +1,23 @@
+package HomeWork.Lesson13;
+
+public class Month {
+
+    public static void dayInMonth(int month){
+        switch (month){
+            case 1:
+                System.out.println("31");
+                break;
+            case 2:
+                System.out.println("28"); break;
+
+            default:
+                System.out.println("30--");
+
+        }
+    }
+
+    public static void main(String[] args) {
+        int month = 1;
+        dayInMonth(6);
+    }
+}
