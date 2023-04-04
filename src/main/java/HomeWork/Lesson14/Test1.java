b7789139437343eceb772b5c6dedccf69f8843ec
chigov2 <chigov@gmail.com>
2023-04-04 12:52:56 +0300

    14

--- src/main/java/HomeWork/Lesson14/Test1.java
+++ src/main/java/HomeWork/Lesson14/Test1.java
@@ -1,9 +1,31 @@
 package HomeWork.Lesson14;
 
 public class Test1 {
-    public static void main(String[] args) {
-        for (int i=0; i < 10; i++){
-            System.out.println("i = "+i);
+
+
+    static void vremya(){
+        OUTER:
+        for (int chas = 0; chas <6; chas++)
+            MIDDLE:
+            for (int minuta=0; minuta <60; minuta++){
+                if (chas > 1 && minuta %10 ==0){
+                    break OUTER;
+                }
+                INNER:
+                for (int secunda = 0; secunda < 60; secunda++){
+                    if (secunda * chas > minuta){
+                        continue MIDDLE;
+                    }
+                    System.out.println(chas + " : "+ minuta + " : "+ secunda );
+                }
+            }
         }
+
+    public static void main(String[] args) {
+       vremya();
     }
-}
+
+    }
+
+
+
