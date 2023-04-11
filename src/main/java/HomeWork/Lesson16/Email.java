4c373c6c04a4da8d5e87e1c7ee5e417138190829
chigov2 <chigov@gmail.com>
2023-04-11 15:03:14 +0300

    16_03

---
+++ src/main/java/HomeWork/Lesson16/Email.java
@@ -0,0 +1,21 @@
+package HomeWork.Lesson16;
+
+public class Email {
+    public void email(String s){
+        int a = 0;//@
+        int b = 0;//.
+        int c = 0;//;
+        while(c < (s.length() - 1)){
+            a = s.indexOf('@',c);
+            b = s.indexOf('.',c);
+            c = s.indexOf(';',c+1 );
+            System.out.println(s.substring(a+1,b));
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Email newEmail = new Email();
+        newEmail.email("ya@yahoo.com; you@ukr.net; on@gmail.com;");
+    }
+}
