5ba8353690893e1ab31cb2828b11c8992a2abe68
chigov2 <chigov@gmail.com>
2023-04-12 16:10:36 +0300

    17

--- src/main/java/HomeWork/Lesson17/Ravenstvo.java
+++ src/main/java/HomeWork/Lesson17/Ravenstvo.java
@@ -11,12 +11,38 @@ if (sb1 == sb2){
 }
     }
 
+    public static boolean ravenstvo2(StringBuilder st1, StringBuilder st2){
+        boolean result = true;
+        if (st1.length() == st2.length()){
+            for (int i = 0; i < st1.length(); i++){
+                if (st1.charAt(i) != st2.charAt(i)){
+                    result = false;
+                    break;
+                }
+
+            }
+        }//else {result = false;}
+        return result;
+    }
+
     public static void main(String[] args) {
+//        StringBuilder ss1 = new StringBuilder("112");
+//        StringBuilder ss2 = new StringBuilder("112");
+//        StringBuilder ss3 = ss1;
+//        Ravenstvo.ravenstvo(ss1,ss2);
+//        Ravenstvo.ravenstvo(ss1,ss3);
         StringBuilder ss1 = new StringBuilder("112");
         StringBuilder ss2 = new StringBuilder("112");
-        StringBuilder ss3 = ss1;
-        Ravenstvo.ravenstvo(ss1,ss2);
-        Ravenstvo.ravenstvo(ss1,ss3);
+        StringBuilder ss3 = new StringBuilder("113");
+
+        boolean a = ravenstvo2(ss1,ss3);
+        System.out.println("a = " +a);
+        boolean b = ravenstvo2(new StringBuilder(),new StringBuilder());
+        System.out.println("b = " +b);
+        boolean c = ravenstvo2(new StringBuilder("f"),new StringBuilder("f"));
+        System.out.println("c = "+c);
+
+
     }
 
 }
