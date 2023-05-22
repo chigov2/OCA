e11f88d1033828f5bced15464826844fce016069
chigov2 <chigov@gmail.com>
2023-05-22 16:11:54 +0300

    28_48

--- src/main/java/HomeWork/Lesson27_Exceptions/Test12.java
+++ src/main/java/HomeWork/Lesson27_Exceptions/Test12.java
@@ -2,4 +2,23 @@ package HomeWork.Lesson27_Exceptions;
 
 public class Test12 {
     //38min
+    public void abc(){
+        int[] array = {1,2,3};
+        try{
+            System.out.println(array[7]);
+        }
+        catch (ArrayIndexOutOfBoundsException error){
+            String s = null;
+            System.out.println(array.length);
+        }
+//        catch (NullPointerException error_null){
+//            System.out.println("null pointer exception");
+//        }
+    }
+
+    public static void main(String[] args) {
+        Test12 t = new Test12();
+        t.abc();
+    }
+
 }
