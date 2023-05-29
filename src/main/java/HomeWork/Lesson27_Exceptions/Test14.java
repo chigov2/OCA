33554444b2c048908293d22e7fd38f80a312ab77
chigov2 <86004573+chigov2@users.noreply.github.com>
2023-05-29 06:46:08 +0300

    49 52 min

--- src/main/java/HomeWork/Lesson27_Exceptions/Test14.java
+++ src/main/java/HomeWork/Lesson27_Exceptions/Test14.java
@@ -1,5 +1,26 @@
 package HomeWork.Lesson27_Exceptions;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class Test14 {
     //49 min
+    static FileInputStream fis1, fis2;
+
+    public static void main(String[] args)  {
+        try{
+            fis1 = new FileInputStream("test9.txt");
+            System.out.println("file test9 does exist and found");
+            try{fis2.close();} catch (IOException e) {
+                System.out.println("Stream problem fis2");
+            }
+        }
+        catch (FileNotFoundException e){
+            System.out.println("file test9 not found");
+        }
+        catch (NullPointerException e) {
+            System.out.println("Null pointer exception works");
+        }
+    }
 }
