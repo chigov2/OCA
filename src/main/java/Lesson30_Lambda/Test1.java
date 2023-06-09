2f53a489b425df7c43b5335177c3e316f4b41c4c
chigov2 <chigov@gmail.com>
2023-06-09 13:18:51 +0300

    30

---
+++ src/main/java/Lesson30_Lambda/Test1.java
@@ -0,0 +1,19 @@
+package Lesson30_Lambda;
+
+public class Test1 {
+}
+class Student{
+    String name;
+    char sex;
+    int age;
+    int course;
+    double evgGrade;
+    Student(String name,char sex,int age, int course,double evgGrade){
+        this.name = name;
+        this.sex = sex;
+        this.age = age;
+        this.course = course;
+        this.evgGrade = evgGrade;
+
+    }
+}
